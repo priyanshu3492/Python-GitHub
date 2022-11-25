3cc0b447027f34e237bf43f1a0511415cadcaf20
PRIYANSHU RANJAN <115810094+priyanshu3492@users.noreply.github.com>
2022-11-25 20:31:34 +0530

    Add files via upload

---
+++ Lecture 11 Comments in Python CipherSchools.py
@@ -0,0 +1,7 @@
+# this is a comment 
+print("Hello World!")
+print('Priyanshu Ranjan') #its my name 
+print('1223492') #its my registration number
+print('KOC09-75') # its my section & roll number
+'''I am learning python coding langaunge from CipherSchools
+'''
