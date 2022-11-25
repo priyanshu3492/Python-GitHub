50ca137ba6dd414c46b6d82af152cba65870747c
PRIYANSHU RANJAN <115810094+priyanshu3492@users.noreply.github.com>
2022-11-25 22:39:18 +0530

    Add files via upload

---
+++ Lecture 19 Chapter 1 Summary in Python CipherSchools.py
@@ -0,0 +1,24 @@
+print('hello world')
+print("hello \"wolrd\"")
+print('lineA \tlineB')
+print('lineA \n lineB')
+print(r'lineA \n lineB')
+print(2*3)
+print(2+3)
+print(2-3)
+print(2**3)
+print(2//3)
+print(2/3)
+print((2+3)*4/2)
+name = "priyanshu"
+print(name)
+name = 123
+print(name)
+_name = (789)
+print(_name)
+p_r = "priyanshu ranjan"
+print(p_r)
+
+
+
+
