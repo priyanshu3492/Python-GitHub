75fa1b2bb760e5273ba79b928377beac8141ccda
PRIYANSHU RANJAN <115810094+priyanshu3492@users.noreply.github.com>
2022-11-28 12:57:37 +0530

    Add files via upload

---
+++ Lecture 30  Step Argument in Python CipherSchools.py
@@ -0,0 +1,15 @@
+# # name = "tomato" #t=0(-6) o=1(-5) m=2(-4) a=3(-3) t=4(-2) o=5(-1)
+# # print(name[3]) #a
+# # print(name[-2]) #t
+
+# name = "tomato"
+# print(name[0:4]) #toma
+# print(name[3:6]) #ato
+# print(name[-3:6]) #ato
+# print(name[2:]) #mato
+# print(name[:3]) #tom
+
+print("Priyanshu"[2:6]) #iyan
+print("Priyanshu"[0:6:2]) #pia step argument
+print("Priyanshu"[0::2]) #piasu(inlude whole input)
+print("Priyanshu"[-1::-1]) #uhsnayirP(reverse)
