fe855e5641e45897231f568dddf3546ab0728fc8
PRIYANSHU RANJAN <115810094+priyanshu3492@users.noreply.github.com>
2022-12-01 22:28:35 +0530

    Add files via upload

---
+++ Lecture 33  String Methods Part 1 in Python CipherSchools.py
@@ -0,0 +1,9 @@
+name = "PrIyAnsHu RaNJan"
+
+print(len(name)) #len function also can taken as variable
+
+print(name.lower()) #samll letter
+print(name.upper()) #capital letter
+print(name.title()) #first letter of word will be capital
+print(name.count("a")) #a=2times
+
