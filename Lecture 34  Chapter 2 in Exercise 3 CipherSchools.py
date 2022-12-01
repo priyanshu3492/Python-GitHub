add5cafba294cd2bde41b1b6f3f339cfabf1ae54
PRIYANSHU RANJAN <115810094+priyanshu3492@users.noreply.github.com>
2022-12-01 22:30:20 +0530

    Add files via upload

---
+++ Lecture 34  Chapter 2 in Exercise 3 CipherSchools.py
@@ -0,0 +1,4 @@
+name,character = input("enter your name and character: ").split(",") #pappu,p
+print(f"length of name: {len(name)}") #5
+print(f"character count avail in name: {name.count(character)}") #p=3 
+
